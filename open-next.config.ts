1d1b60a7334b29286592d13611e200b5097aa8c6
omegadevelopmentsfr <omegadevelopmentsfr@gmail.com>
2026-05-05 21:48:13 +0200

    fix: manual cloudflare config with next 14.2.35 and build flag

---
+++ open-next.config.ts
@@ -0,0 +1,23 @@
+import type { OpenNextConfig } from "@opennextjs/aws/types/open-next.js";
+
+const config = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+} satisfies OpenNextConfig;
+
+export default config;
