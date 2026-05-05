d2ac3ab7318802f424240fc1083b162a013238f9
omegadevelopmentsfr <omegadevelopmentsfr@gmail.com>
2026-05-05 21:50:24 +0200

    fix: remove type import in config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,3 @@
-import type { OpenNextConfig } from "@opennextjs/aws/types/open-next.js";
-
 const config = {
   default: {
     override: {
@@ -18,6 +16,6 @@ const config = {
       proxyExternalRequest: "fetch",
     },
   },
-} satisfies OpenNextConfig;
+};
 
 export default config;
