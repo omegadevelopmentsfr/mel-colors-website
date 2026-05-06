5295c251f55e3a54b16b2c1e7dd825ed18b50df6
omegadevelopmentsfr <omegadevelopmentsfr@gmail.com>
2026-05-06 14:56:03 +0200

    build: upgrade Next.js to v15 and migrate to @opennextjs/cloudflare CLI commands

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,22 +1,3 @@
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-    },
-  },
-};
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default config;
+export default defineCloudflareConfig();
